099a604a5087531d14928f4aebfd14d4a8a68ee8
shimaazizi <shima.azizimajd98@gmail.com>
2024-08-07 13:08:37 +0330

    add server.py for fast api

---
+++ FastAPI/server.py
@@ -0,0 +1,23 @@
+import io 
+from segmentation import segmentator, get_segments
+from starlette.responses import Response
+
+from fastapi import FastAPI, File
+
+import uvicorn
+
+model = segmentator()
+
+
+app = FastAPI(
+    title="predict mask"
+)
+
+@app.post("/image segmentation")
+async def get_segmentation_map(file: bytes = File(...)):
+        """Get segmentation maps from image file"""
+        segmented_image = get_segments(model, file)
+        bytes_io = io.BytesIO()
+        segmented_image.save(bytes_io, format="PNG")
+        return Response(bytes_io.getvalue(), media_type="image/png")
+
